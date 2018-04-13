3ad997c0c312e725db50279f7ea6868c6752a21a
JessAskeland <36720058+JessAskeland@users.noreply.github.com>
2018-04-13 11:58:37 -0500

    jj

--- src/main/java/beans/VolunteerAccountDaoJpaImpl.java
+++ src/main/java/beans/VolunteerAccountDaoJpaImpl.java
@@ -1,5 +1,47 @@
 package beans;
 
-public class VolunteerAccountDaoJpaImpl {
+import java.util.List;
+
+public class VolunteerAccountDaoJpaImpl implements VolunteerAccountDao {
+
+	public void insert(VolunteerAccount volunteerAccount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update(VolunteerAccount volunteerAccount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update(List<VolunteerAccount> VolunteerAccounts) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void delete(long volunteerAccountId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public VolunteerAccount find(long volunteerAccountId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<VolunteerAccount> find(List<Long> volunteerAccountIds) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<VolunteerAccount> find(String volunteerName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<VolunteerAccount> find(boolean locked) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
